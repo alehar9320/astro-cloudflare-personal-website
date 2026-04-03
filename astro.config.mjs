ee637d665b54039e5dc4ed1c315c8cf48a5362f0
Alexander Harenstam <alehar9320@users.noreply.github.com>
2026-04-03 15:12:43 +0200

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry integration to the Astro configuration for enhanced error tracking.
    - Updated package dependencies to include @sentry/astro and @sentry/cloudflare.
    - Modified compatibility flags in wrangler configuration for improved functionality.
    
    This integration will help in monitoring application performance and capturing errors effectively.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+  integrations: [
+    sentry({
+      dsn: 'https://7ecb3a9c0549d3d1818112e1bc11ad67@o4511135179669504.ingest.de.sentry.io/4511155924435024',
+      tracesSampleRate: 1.0,
+      enableLogs: true,
+      sendDefaultPii: true,
+      sourceMapsUploadOptions: {
+        org: 'alexander-m4',
+        project: 'node-cloudflare-workers',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
 });
